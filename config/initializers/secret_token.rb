d7b1d6b59498d6ccbd8ee299c036d4e2b78aeb6b
Kazunori Fujimoto <kazu-f@sd5.so-net.ne.jp>
2014-03-26 14:41:43 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwilioAgentBrowser::Application.config.secret_key_base = '94887069e7c0841b8840e272b0d1f0d812311f1a07686d315ba6612703484560fef550527d30c6eb66924f9211daf6bc7e57088ecac4c5a9b6fdeccefff51224'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TwilioAgentBrowser::Application.config.session_store :cookie_store, key: '_twilio_agent_browser_session'
