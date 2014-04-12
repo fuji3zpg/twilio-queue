324de05d0a104bf17d2f66b3a4353756d93548fb
Kazunori Fujimoto <kazu-f@sd5.so-net.ne.jp>
2014-04-12 10:16:10 +0900

    chnage call#voice HTTP method from post to get

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 TwilioAgentBrowser::Application.routes.draw do
   get 'call/client'
-  post 'call/voice'
+  get 'call/voice'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
